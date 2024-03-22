6edd6daef76dc67000d8b8dc3d748f474581c193
MARCUS VINICIUS COSTA DA SILVA <marcus.vsilva@a.ucb.br>
2024-03-22 10:11:50 -0300

    atividade

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+console.log('Bem vindo a operação')
+let a = 4;
+let b = 3;
+
+let ope = a + b;
+console.log('valor da operação somada:', ope);
+ope = a - b
+console.log('valor da operação subtraida:', ope);
+ope = a * b
+console.log('valor da operação multiplicada:', ope);
+ope = a / b;
+console.log('valor da operação dividida:', ope);
